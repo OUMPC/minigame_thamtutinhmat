c4d6d36706da9fec0bdc12ad0b11bcf715d607a6
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2024-12-21 00:07:38 +0700

    final 23

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
