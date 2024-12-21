f441b19ca380a66a3674cd659293fd30cd7b5455
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2024-12-22 03:09:45 +0700

    A

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 let db;
+const dbPath = path.join(__dirname, 'data', 'leaderboard.db');
+const tmpDbPath = '/tmp/leaderboard.db';
+
 if (process.env.NODE_ENV === 'production') {
-    db = new sqlite3.Database('/tmp/leaderboard.db');
-    fs.copyFileSync(path.join(__dirname, 'data', 'leaderboard.db'), '/tmp/leaderboard.db');
+    fs.copyFileSync(dbPath, tmpDbPath);
+    db = new sqlite3.Database(tmpDbPath);
 } else {
-    const dbPath = path.join(__dirname, 'data', 'leaderboard.db');
     db = new sqlite3.Database(dbPath);
 }
 
@@ -127,3 +129,4 @@ app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
+ 
